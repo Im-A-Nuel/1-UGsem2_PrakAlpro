cd7518149a3b45367db1a131b4ac7f4aecc819e4
Imanuel <118039925+Im-A-Nuel@users.noreply.github.com>
2023-02-09 18:25:24 +0700

    Update 71220927-01.py

--- 71220927-01.py
+++ 71220927-01.py
@@ -8,13 +8,19 @@ menit_akhr=int(input("menit_akhir = "))
 #Proses
 jamawal=jam_awl*60
 jamakhr=jam_akhr*60
-waktu_awal=jamawal+menit_awl
-waktu_akhir=jamakhr+menit_akhr
-selisih=waktu_awal-waktu_akhir
+
+if menit_awl < menit_akhr:
+    hasil=(jamakhr-jamawal)+(menit_akhr-menit_awl)
+else:
+    hasil=(jamakhr-jamawal)+(60+(menit_akhr-menit_awl))
 
 print("")
 #Output
-print("selisihnya adalah",selisih,"menit")
+print("selisih antara",str(jam_awl)+":"+str(menit_awl),"dan",str(jam_akhr)+":"+str(menit_akhr),"adalah", hasil,"menit")
+
+
+
+
 
 
 
